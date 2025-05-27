dbce425c0ee420caac4eecabeff2ef9f3da28c75
Giorgia Valenti <giorgia.valenti@eds-org.it>
2025-05-27 17:34:25 +0200

    Get the transactions and totals

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import csv
+from datetime import datetime
 from data_entry import get_amount, get_category, get_date, get_description
 
 class CSV:
     CSV_FILE = "finance_data.csv"
     COLUMNS = ["date", "amount", "category", "description"]
+    DATE_FORMAT = "%d-%m-%Y"
 
     @classmethod
     def initialize_csv(cls):
@@ -26,6 +28,29 @@ class CSV:
             writer = csv.DictWriter(csvfile, fieldnames=cls.COLUMNS)
             writer.writerow(new_entry)
         print("Entry added successfully")
+    
+    @classmethod
+    def get_transactions(cls, start_date, end_date):
+        df = pd.read_csv(cls.CSV_FILE)
+        df["date"] = pd.to_datetime(df["date"], format=CSV.DATE_FORMAT)
+        start_date = datetime.strptime(start_date, CSV.DATE_FORMAT)
+        end_date = datetime.strptime(end_date, CSV.DATE_FORMAT)
+
+        mask = (df["date"] >= start_date) & (df["date"] <= end_date)
+        filtered_df = df.loc[mask]
+
+        if filtered_df.empty:
+            print('No transactions found in the given date range')
+        else:
+            print(f"Transactions from {start_date.strftime(CSV.DATE_FORMAT)} to {end_date.strftime(CSV.DATE_FORMAT)}")
+            print(filtered_df.to_string(index=False, formatters={"date": lambda x: x.strftime(CSV.DATE_FORMAT)}))
+
+            total_income = filtered_df[filtered_df["category"] == "Income"]["amount"].sum()
+            total_expense = filtered_df[filtered_df["category"] == "Expense"]["amount"].sum()
+            print("\nSummary:")
+            print(f"Total Income: ${total_income:.2f}")
+            print(f"Total Expense: ${total_expense:.2f}")
+            print(f"Net Savings: ${(total_income - total_expense):.2f}")
 
 def add():
     CSV.initialize_csv()
@@ -35,4 +60,5 @@ def add():
     description = get_description()
     CSV.add_entry(date, amount, category, description)
 
+CSV.get_transactions("25-05-2025","29-05-2025")
 add()
